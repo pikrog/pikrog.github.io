06dd461b55baa111ac9b605bbd8ae70738cd44df
pikrog <piotrekkrogulski@gmail.com>
2025-12-26 17:52:52 +0100

    show oauth token

---
+++ index.js
@@ -0,0 +1,10 @@
+const codeParamName = 'code';
+const tokenSpanId = 'token';
+
+const tokenSpan = document.getElementById(tokenSpanId);
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const code = params.get('code');
+
+tokenSpan.innerText = code;
